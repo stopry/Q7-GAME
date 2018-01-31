cc12dea2ed9ccb4bb0f4af7e956f9313f73b84a6
stopry <809746561@qq.com>
2018-01-31 18:16:31 +0800

    a

--- assets/scripts/global/LittleTip.js
+++ assets/scripts/global/LittleTip.js
@@ -17,20 +17,24 @@ cc.Class({
         this.root = cc.find('Canvas');
     },
     setContent:function(str){//设置提示内容
-        var _little = cc.instantiate(this.tipPrefab);
+        if(!Global.tips||!Global.tips.name){
+            Global.tips = cc.instantiate(this.tipPrefab);
+        }
+        Global.tips.setSiblingIndex(-1);
+        Global.tips.stopAllActions();
+
+        var finished = cc.callFunc(()=>{Global.tips.active = false}, this);
+
         var actionShow = cc.fadeTo(0.2,255);
         var actionHold = cc.fadeTo(1.5,255);
         var actionHide = cc.fadeTo(0.5,0);
-        var seq = cc.sequence(actionShow,actionHold,actionHide);
-        _little.getChildByName('littleTip').getComponent(cc.RichText).string = "<color=#B23214><outline color=#FFECB4 width=2>"+str+"</outline></color>";
-        _little.parent = this.root;
-        _little.runAction(seq);
-        _little.on(cc.Node.EventType.TOUCH_END,()=>{
-            if(_little){
-                _little.stopAllActions();
-                _little.destroy();
-            }
-            //_little.active = false;
+        var seq = cc.sequence(actionShow,actionHold,actionHide,finished);
+        Global.tips.getChildByName('littleTip').getComponent(cc.RichText).string = "<color=#B23214><outline color=#FFECB4 width=2>"+str+"</outline></color>";
+        Global.tips.parent = cc.find('Canvas');
+        Global.tips.active = true;
+        Global.tips.runAction(seq);
+        Global.tips.on(cc.Node.EventType.TOUCH_END,()=>{
+            Global.tips.active = false;
         },this);
     }
     // called every frame, uncomment this function to activate update callback
